c46bff5f3f6ee912ae7db978644002a6f173b351
CoreyMcCann <coreymccann4@gmail.com>
2024-05-20 15:15:47 -0700

    interface update

--- interface/root_component.py
+++ interface/root_component.py
@@ -5,6 +5,7 @@ from connectors.binance_futures import BinanceFuturesClient
 
 from interface.styling import *
 from interface.logging_component import Logging
+from interface.watchlist_component import Watchlist
 
 
 class Root(tk.Tk):
@@ -22,6 +23,9 @@ class Root(tk.Tk):
 
         self._right_frame = tk.Frame(self, bg=BG_COLOR)
         self._right_frame.pack(side=tk.LEFT)
+
+        self._watchlist_frame = Watchlist(self.binance.contracts, self.bitmex.contracts, self._left_frame, bg=BG_COLOR)
+        self._watchlist_frame.pack(side=tk.TOP)
 
         self._logging_frame = Logging(self._left_frame, bg=BG_COLOR)
         self._logging_frame.pack(side=tk.TOP)
